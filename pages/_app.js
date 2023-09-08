7ba355aae8082f5fdae7de8ebf47c0d61def1763
AZIZ <norn91333@gmail.com>
2023-09-08 11:42:02 +0100

    ads

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/index.css";
 import { Fragment } from "react";
 import { DefaultSeo } from "next-seo";
+import Head from "next/head"; // Import the Head component
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -16,9 +17,18 @@ function MyApp({ Component, pageProps }) {
           site_name: "Youtube Thumbnail Downloader",
         }}
       />
+      {/* Include the Google AdSense script in the head section */}
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4644191924023931"
+          crossorigin="anonymous"
+        ></script>
+      </Head>
       <Component {...pageProps} />
     </Fragment>
   );
 }
 
 export default MyApp;
+
